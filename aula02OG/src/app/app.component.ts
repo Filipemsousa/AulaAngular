68c02fd19b7da6bee6a597b05cf215c571e78cd4
Filipemsousa <filipmenzessousa@gmail.com>
2025-11-07 11:38:52 -0300

    criacao de site em angular

--- aula02OG/src/app/app.component.ts
+++ aula02OG/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./componentes/header/header.component";
+import { FooterComponent } from "./componentes/footer/footer.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { SaboresComponent } from "./pages/sabores/sabores.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, SaboresComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- aula02OG/src/app/app.routes.ts
+++ aula02OG/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SaboresComponent } from './pages/sabores/sabores.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"home",component:HomeComponent},
+    {path:"sabores",component:SaboresComponent},
+    {path:"",redirectTo:"home",pathMatch:"full"}
+];
